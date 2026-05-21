c8a8c37c9fe627e199ef112bc5431278dac5dcda
Pysrdafoice <andersondafoice@gmail.com>
2026-05-21 11:35:36 -0300

    formulario está apresentando as informações, mas não tem validação

--- js/form.js
+++ js/form.js
@@ -11,22 +11,11 @@ class contato {
         this.contato = contato;
     }
     
-    console() {
-        console.log("Nome: " + this.nome);
-        console.log("Sobrenome: " + this.sobrenome);
-        console.log("Email: " + this.email);
-        console.log("CPF: " + this.cpf);
-        console.log("Telefone: " + this.telefone);
-        console.log("Contato: " + this.contato);
-    }
-
     
 }
 
 
-
 function Post(form) {
-
 
   let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
@@ -34,24 +23,21 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
+
+            
+            console.table(data);
+             Enviar();
 }
 
 
 function Enviar() {
 
-    var nome = document.getElementById("nomeid");
-    // let objeto = new contato("nome", "sobrenome", "email", "cpf", "telefone", "contato");
-    //     objeto.console();
-
-    // let nome = document.getElementById("nome").value;
-    // let sobrenome = document.getElementById("sobrenome").value;
-    // let email = document.getElementById("email").value;
-    // let cpf = document.getElementById("cpf").value;
-    // let telefone = document.getElementById("telefone").value;
-    // let contato = document.getElementById("contato").value;
-}
-    
+    let nome = document.getElementById("nomeid");
     if (nome.value != "") {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    } return;
-
+    }
+   
+}
+    
+    
+    

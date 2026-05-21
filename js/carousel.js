37edd959bc2fc4150fadc9210dc78674924679bf
Pysrdafoice <andersondafoice@gmail.com>
2026-05-21 09:23:20 -0300

    os botoes esquerdo e direito estão funcionando e resetam a contagem a cada vez que passam

--- js/carousel.js
+++ js/carousel.js
@@ -15,21 +15,21 @@ class Carousel {
     }
 
     static Start(arr) {
-
-        if (arr) {
-            if (arr.length > 0) {
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel._items = arr;
-                Carousel.Proximo(); // start
-                Carousel._interval = setInterval(function () { Carousel.Proximo(); }, 5000);
-            }
-        } else {
-            throw "Method Start need a Array Variable.";
+        
+         if (Carousel._interval) {
+        clearInterval(Carousel._interval);
+    }
+        
+        if (arr.length > 0) {
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel._items = arr;
+            Carousel.Mostrar();
+            Carousel._interval = setInterval(function () { Carousel.Proximo(); }, 5000);
         }
     }
 
-    static Proximo() {
+    static Mostrar() {
         const item = Carousel._items[Carousel._sequence];
         const container = document.getElementById('carousel');
         const titleContainer = document.getElementById('carousel-title');
@@ -38,34 +38,60 @@ class Carousel {
             container.innerHTML = '<img src="img/' + item.image + '" alt="' + item.title + '" />';
             titleContainer.textContent = item.title;
         }
-
-        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
-
-        
-        }
     }
+
+    static Proximo() {
+        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+        Carousel.Mostrar();
+    }
+}
        
 
 document.addEventListener('DOMContentLoaded', function () {
     Carousel.Start(carouselArr);
 
-    const button_direito = document.getElementById('butao_direito');
-    const button_esquerdo = document.getElementById('butao_esquerdo');
+    const button_direito = document.querySelector('.butao_direito');
+    const button_esquerdo = document.querySelector('.butao_esquerdo');
 
-    if(button_direito) {
-        button_direito.addEventListener('click', function () {
-            index = index + 1 % Carousel._size;
-            clearInterval(Carousel._interval);
-            Carousel.Proximo();
-        });
+    if (button_direito) {
+        button_direito.onclick = function () {
+            botao_direito();
+        };
     }
-    if(button_esquerdo) {
-        index = --index < 0 ? Carousel._size - 1 : index;
-        button_esquerdo.addEventListener('click', function () {
-            clearInterval(Carousel._interval);
-            Carousel.Proximo();
-        });
+
+    if (button_esquerdo) {
+        button_esquerdo.onclick = function () {
+            botao_esquerdo();
+        };
     }
 });
+
+function botao_direito() {
+    if (!Carousel._items || !Carousel._size) {
+        return;
+    }
+
+    if (Carousel._interval) {
+        clearInterval(Carousel._interval);
+    }
+
+    Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
+    Carousel.Mostrar();
+    Carousel._interval = setInterval(function () { Carousel.Proximo(); }, 5000);
+}
+
+function botao_esquerdo() {
+    if (!Carousel._items || !Carousel._size) {
+        return;
+    }
+
+    if (Carousel._interval) {
+        clearInterval(Carousel._interval);
+    }
+
+    Carousel._sequence = (Carousel._sequence - 1 + Carousel._size) % Carousel._size;
+    Carousel.Mostrar();
+    Carousel._interval = setInterval(function () { Carousel.Proximo(); }, 5000);
+}
 
 
